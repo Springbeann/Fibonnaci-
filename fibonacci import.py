c80bc30522b3de2740103c5782dac7a2738ea867
Kamili Saintleger <77054888+Springbeann@users.noreply.github.com>
2021-11-12 00:35:43 -0500

    Fibonnaci
    This is an exercise on relating the Fibonnaci sequence to recursion

---
+++ fibonacci import.py
@@ -0,0 +1,13 @@
+from fibonnaci import Fibonacci 
+
+fibonacci_of = Fibonacci()
+
+fibonacci_of(5)
+
+fibonacci_of(6)
+
+fibonacci_of(7)
+
+[fibonacci_of(n) for n in range(15)]
+
+print(fibonacci_of(6))
